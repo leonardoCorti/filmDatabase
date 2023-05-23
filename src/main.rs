bb1d30eeb24d7bda56133e52221e0aec01505855
leonardoCorti <leonardo.filippo@ymail.com>
2023-05-23 17:24:42 +0200

    refactor top row

--- src/main.rs
+++ src/main.rs
@@ -28,28 +28,32 @@ fn main() {
 
 fn root_widget() -> impl Widget<HelloState> {
 
-    let textbox = TextBox::new()
-        .with_placeholder("What film did you watch?")
-        .expand_width()
-        .lens(HelloState::name)
-        ;
-    let button = Button::new("add film")
-        .fix_width(100.)
-        .on_click(|_, data: &mut HelloState, _: &_| ());
-    let text_placeHolder = TextBox::new()
+   let text_placeHolder = TextBox::new()
         .with_placeholder("placeholder")
         .lens(HelloState::name);
 
     let layout = Flex::column()
-        .with_child(
-            Flex::row()
-                .with_flex_child(textbox, 1.0)
-                .with_child(button)
-                .cross_axis_alignment(druid::widget::CrossAxisAlignment::Fill)
-                .fix_height(30.)
-                .padding(10.)
-        )
-        .with_flex_child(text_placeHolder, 1.0)
-        ;
+        .with_child(top_bar())
+        .with_flex_child(text_placeHolder, 1.0);
+
     Align::centered(layout)
+}
+
+fn top_bar() -> impl Widget<HelloState> + 'static {
+    
+    let textbox = TextBox::new()
+        .with_placeholder("What film did you watch?")
+        .expand_width()
+        .lens(HelloState::name);
+
+    let button = Button::new("add film")
+        .fix_width(100.)
+        .on_click(|_, data: &mut HelloState, _: &_| ());
+
+    Flex::row()
+        .with_flex_child(textbox, 1.0)
+        .with_child(button)
+        .cross_axis_alignment(druid::widget::CrossAxisAlignment::Fill)
+        .fix_height(30.)
+        .padding(10.)
 }
