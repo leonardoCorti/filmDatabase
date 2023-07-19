b4706936e31e6b84227995225bfba0ab6065fdf1
leonardoCorti <leonardo.filippo@ymail.com>
2023-07-19 17:07:34 +0200

    added search and info functionality with omdb

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 a simple gui application to create a small film database to keep track of the films you watched and have some data
 
 WIP!
+
+I'm using OMDb, to use this app you will need an API key, it's free 
--- src/Film.rs
+++ src/Film.rs
@@ -1,15 +1,48 @@
 use serde::{Serialize, Deserialize};
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Film{
+    Title: String,
+    Year: String,
+    Released: String,
+    Runtime: String,
+    Genre: String,
+    Metascore: String,
+    Poster: String,
+}
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Film {
-    title: String,
-    year: u32,
-    image: String,
-    runtime: String,
-    plot: String,
-    director: String,
-    stars: String,
-    geners: String,
-    rating: u16,
+pub struct FilmFromListOmdb {
+    Title: String,
+    Year: String,
+    imdbID: String,
+    Poster: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FilmList {
+    Search: Vec<FilmFromListOmdb>,
+    totalResults: String,
+}
+
+pub fn search_film(name: &str, api: &str) -> FilmList {
+    let base_url = "http://www.omdbapi.com/";
+    let url = format!("{}/?apikey={}&s={}",base_url,api,name.replace(" ", "%20") );
+    println!("{}", url);
+    let response = reqwest::blocking::get(url).unwrap();
+    let response_string = response.text().unwrap();
+
+    let results: FilmList = serde_json::from_str(&response_string).unwrap();
+    results
+}
+
+pub fn film_info(id: &str, api: &str) -> Film {
+    let base_url = "http://www.omdbapi.com/";
+    let url = format!("{}/?apikey={}&i={}",base_url,api,id );
+    println!("{}", url);
+    let response = reqwest::blocking::get(url).unwrap();
+    let response_string = response.text().unwrap();
+
+    let result: Film = serde_json::from_str(&response_string).unwrap();
+    result 
 }
